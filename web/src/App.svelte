975b1f9644f76a9f44eb9e84c507c50364d36550
Jimmy Wei <ijimmywei@gmail.com>
2020-07-25 13:49:59 +0100

    Basic svelte frontend scaffold

---
+++ web/src/App.svelte
@@ -0,0 +1,39 @@
+
+<script lang="ts">
+	import ApolloClient from "apollo-boost";
+	import { setClient, query } from "svelte-apollo";
+
+	export let name: string;
+
+	const client: any = new ApolloClient({uri: "localhost:8085/query"});
+	setClient(client);
+
+	const books = query(client, { query: GET_BOOKS });
+</script>
+
+<main>
+	<h1>derp</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
